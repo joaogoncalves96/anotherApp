1f862e4244f4e982da312117ad56e097618b70e1
joaogoncalves96 <joao_pcg@hotmail.com>
2022-05-30 13:03:23 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react'
+
+import Rotas from "@joaogoncalves96/form/lib/routes/rotas"
+// import Login from "@joaogoncalves96/form/lib/components/Login/Login"
+
+
+
+function App() {
+  return (
+    <div style={ {background: "pink"}}>
+      <Rotas />
+    </div>
+  );
+}
+
+export default App;
